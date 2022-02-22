9d50219e29b6c751c73d823924c450c276d2e95a
Preston Rozwood <279pdbz199@gmail.com>
2022-02-21 19:27:44 -0800

    Includes buying/selling circuit

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -8,53 +8,106 @@ contract Registry is IRegistry {
     mapping(address => string) private addressToUsername;
     mapping(string => address) private usernameToAddress;
     mapping(string => bool) private usernameExists;
+    mapping(string => uint256) private usernamePrices;
 
     // ERRORS
     error UsernameAlreadyExists(string username);
     error UsernameCannotBeEmptyString();
     error AccountDoesNotHaveAUsername(address account);
     error UsernameHasNotBeenSet(string username);
+    error UsernameHasNotBeenListed();
+    error PriceNotHighEnoughToPurchaseUsername(uint256 price, string username);
+    error FailedToSendCrypto();
 
     // MODIFIERS
-    modifier checkUsername(string calldata _username) {
+    modifier usernameDoesNotExist(string calldata _username) {
         if (usernameExists[_username]) {
             revert UsernameAlreadyExists(_username);
         }
+        _;
+    }
+
+    modifier checkUsername(string calldata _username) {
         if (bytes(_username).length == 0) {
             revert UsernameCannotBeEmptyString();
         }
         _;
     }
 
-    modifier checkAccount(address _account) {
+    modifier accountHasUsername(address _account) {
         if (!usernameExists[addressToUsername[_account]]) {
             revert AccountDoesNotHaveAUsername(_account);
         }
         _;
     }
 
+    modifier usernameIsListed(string memory _username) {
+        if (usernamePrices[_username] == 0) {
+            revert UsernameHasNotBeenListed();
+        }
+        _;
+    }
+
     // FUNCTIONS
-    function setUsername(string calldata _username) external checkUsername(_username) {
+    function setUsername(string calldata _username) external
+        usernameDoesNotExist(_username)
+        checkUsername(_username)
+    {
         usernameExists[addressToUsername[tx.origin]] = false;
         usernameExists[_username] = true;
+
+        usernameToAddress[addressToUsername[tx.origin]] = address(0);
         usernameToAddress[_username] = tx.origin;
+        
         addressToUsername[tx.origin] = _username;
     }
 
     function removeUsername() external
-        checkAccount(tx.origin)
+        accountHasUsername(tx.origin)
     {
         usernameExists[addressToUsername[tx.origin]] = false;
         usernameToAddress[addressToUsername[tx.origin]] = address(0);
         addressToUsername[tx.origin] = "";
     }
 
-    function getUsername(address _account) external view returns(string memory) {
-        string memory username = addressToUsername[_account];
-        if (bytes(username).length == 0) {
-            revert AccountDoesNotHaveAUsername(tx.origin);
+    function listUsername(
+        uint256 _price
+    ) external accountHasUsername(tx.origin) {
+        usernamePrices[addressToUsername[tx.origin]] = _price;
+    }
+
+    function listingPrice(string calldata _username) external view returns(uint256) {
+        return usernamePrices[_username];
+    }
+
+    function unlistUsername() external
+        usernameIsListed(addressToUsername[tx.origin])
+    {
+        usernamePrices[addressToUsername[tx.origin]] = 0;
+    }
+
+    function buyUsername(
+        string calldata _username
+    ) external payable usernameIsListed(_username) {
+        if (msg.value < usernamePrices[_username]) {
+            revert PriceNotHighEnoughToPurchaseUsername(msg.value, _username);
         }
-        return username;
+
+        bool success = payable(usernameToAddress[_username]).send(msg.value);
+        if (!success) {
+            revert FailedToSendCrypto();
+        }
+
+        usernamePrices[_username] = 0;
+        addressToUsername[usernameToAddress[_username]] = "";
+        usernameToAddress[_username] = tx.origin;
+        addressToUsername[tx.origin] = _username;
+    }
+
+    function getUsername(
+        address _account
+    ) external accountHasUsername(_account) view returns(string memory) {
+        return addressToUsername[_account];
     }
 
     function getAddress(string calldata _username) external view returns(address) {
--- test/Registry.test.ts
+++ test/Registry.test.ts
@@ -23,6 +23,13 @@ describe('Registry', async () => {
         expect(await registry.getUsername(signerAddresses[0])).to.equal('Joe.eth')
         expect(await registry.getAddress('Joe.eth')).to.equal(signerAddresses[0])
     })
+    it('overwrites username', async () => {
+        await registry.setUsername('Joe.eth')
+        await registry.setUsername('JOE.eth')
+        expect(registry.getAddress('Joe.eth')).to.be.revertedWith('UsernameHasNotBeenSet')
+        expect(await registry.getUsername(signerAddresses[0])).to.equal('JOE.eth')
+        expect(await registry.getAddress('JOE.eth')).to.equal(signerAddresses[0])
+    })
     it('changes username', async () => {
         await registry.setUsername('Joe.eth')
         expect(await registry.getUsername(signerAddresses[0])).to.equal('Joe.eth')
@@ -56,5 +63,48 @@ describe('Registry', async () => {
     it('cannot remove username before creating one', async () => {
         expect(registry.removeUsername())
             .to.be.revertedWith('AccountDoesNotHaveAUsername')
+    })
+    it('list username for purchase', async () => {
+        await registry.setUsername('Joe.eth')
+        await registry.listUsername(ethers.utils.parseEther('0.2'))
+        expect(await registry.listingPrice('Joe.eth')).to.equal(ethers.utils.parseEther('0.2'))
+    })
+    it('list username and sell to other user', async () => {
+        await registry.setUsername('Joe.eth')
+        expect(await registry.getUsername(signerAddresses[0])).to.equal('Joe.eth')
+        await registry.listUsername(ethers.utils.parseEther('0.2'))
+
+        const signer0Balance = await signers[0].getBalance()
+        const signer1Balance = await signers[1].getBalance()
+
+        const buyTx = await registry.connect(signers[1]).populateTransaction.buyUsername(
+            'Joe.eth',
+            {
+                value: ethers.utils.parseEther('0.2')
+            }
+        )
+        const txReceipt = await (await signers[1].sendTransaction(buyTx)).wait()
+        expect(registry.getUsername(signerAddresses[0])).to.be.revertedWith('AccountDoesNotHaveAUsername')
+        expect(await registry.getUsername(signerAddresses[1])).to.equal('Joe.eth')
+        expect(await signers[0].getBalance()).to.equal(
+            signer0Balance
+                .add(ethers.utils.parseEther('0.2'))
+        )
+        expect(await signers[1].getBalance()).to.equal(
+            signer1Balance
+                .sub(ethers.utils.parseEther('0.2'))
+                .sub(txReceipt.cumulativeGasUsed.mul(txReceipt.effectiveGasPrice))
+        )
+    })
+    it('list username and unlist', async () => {
+        await registry.setUsername('Joe.eth')
+        await registry.listUsername(ethers.utils.parseEther('0.2'))
+        expect(await registry.listingPrice('Joe.eth')).to.equal(ethers.utils.parseEther('0.2'))
+        await registry.unlistUsername()
+        expect(await registry.listingPrice('Joe.eth')).to.equal(ethers.BigNumber.from('0'))
+    })
+    it('cannot purchase unlisted username', async () => {
+        expect(registry.connect(signers[1]).buyUsername('Joe.eth'))
+            .to.be.revertedWith('UsernameHasNotBeenListed')
     })
 })
